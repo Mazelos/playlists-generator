385decf1e296a773a3af4a92183c52b4b2481568
Madzelos <madze706@gmail.com>
2020-05-25 01:07:59 +0200

    added render user info to display

--- src/client/components/UserInfo/UserInfo.js
+++ src/client/components/UserInfo/UserInfo.js
@@ -5,19 +5,34 @@ export class UserInfo extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      info: {}
+      info: {},
+      infoAreHidden: true
     }
     this.logInfo = this.logInfo.bind(this);
   }
 
   logInfo() { 
-    console.log('user info : \n', this.props.userInfo)
+    this.setState({
+      infoAreHidden: !this.state.infoAreHidden
+    })
   }
 
   render() {
     return (
-      <div className='info-button'>
-        <a onClick={this.logInfo}>{this.props.userInfo.id}</a>
+      <div>
+        <div className='info-button' onClick={this.logInfo}>
+          <a>{this.props.userInfo.id}</a>
+        </div>
+        <div className={`info-container ${this.state.infoAreHidden? 'hidden' : 'visible'}`} >
+          <h3>{this.props.userInfo.display_name}</h3>
+          <p><strong>Email</strong>: {this.props.userInfo.email}</p>
+          <p><strong>Country</strong>: {this.props.userInfo.country}</p>
+          <p><strong>Account status</strong>: {this.props.userInfo.product}</p>
+          {/*this will prevent React to throw an error, (react renders twice...)*/}
+          <a href={this.props.userInfo.external_urls ? this.props.userInfo.external_urls.spotify : ''} target='blank'>
+            <strong>Spotify web player</strong>
+          </a>
+        </div>
       </div>
     )
   }
